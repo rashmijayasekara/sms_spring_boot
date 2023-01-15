b0fa61b643b57e66400722d43563576208286470
Rashmi <rashmijayasekara00@gmail.com>
2023-01-15 13:01:19 +0530

    Save Students

---
+++ src/main/java/com/example/demo/student/StudentConfig.java
@@ -0,0 +1,36 @@
+package com.example.demo.student;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@Configuration
+public class StudentConfig {
+
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository repository){
+        return args -> {
+            Student rashmi=new Student(
+                    "Rashmi",
+                    "rashmijayasekara7899@gmail.com",
+                    LocalDate.of(1997, Month.JANUARY,30),
+                    25
+            );
+            Student nishani=new Student(
+                    "Nishani",
+                    "nishani@gmail.com",
+                    LocalDate.of(1997,Month.JANUARY,12),
+                    25
+            );
+
+            repository.saveAll(
+                    List.of(rashmi,nishani)
+            );
+
+        };
+    }
+}
