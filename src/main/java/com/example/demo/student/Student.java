040be969095d56092a119df4baceffd2b93293aa
Rashmi <rashmijayasekara00@gmail.com>
2023-01-15 11:06:21 +0530

    Add the API layer

---
+++ src/main/java/com/example/demo/student/Student.java
@@ -0,0 +1,23 @@
+package com.example.demo.student;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data @AllArgsConstructor @NoArgsConstructor
+public class Student {
+    private Long id;
+    private String name;
+    private String email;
+    private LocalDate dob;
+    private Integer age;
+
+    public Student(String name, String email, LocalDate dob, Integer age) {
+        this.name = name;
+        this.email = email;
+        this.dob = dob;
+        this.age = age;
+    }
+}
---
+++ src/main/java/com/example/demo/student/StudentController.java
@@ -0,0 +1,19 @@
+package com.example.demo.student;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/students")
+public class StudentController {
+    @GetMapping
+    public List<Student> getStudents(){
+        return List.of(
+                new Student(1L,"mariam","jayasekara7899@gmail.com", LocalDate.of(2000,9,23),21)
+        );
+    }
+}
